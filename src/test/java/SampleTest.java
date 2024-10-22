196cec52506cae50d855882e2969fca79fa467f9
bocdar <erdembocdar@gmail.com>
2024-10-22 05:00:20 +0300

    First Commit

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,24 +1,36 @@
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 class SampleTest {
     @Test
     void openGoogle() {
-        System.setProperty("webdriver.chrome.driver", "C:/Program Files/ChromeDriver/chrome.exe");
+        // WebDriverManager ile ChromeDriver'ı otomatik indir ve ayarla
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "C:/Program Files/ChromeDriver/chrome.exe");
         WebDriver driver = new ChromeDriver();
-        try {
-            driver.get("https://www.google.com");
-            System.out.println("Title: " + driver.getTitle());
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        } finally {
-            try {
-                driver.quit();
-            } catch (Exception e) {
-                System.out.println("Error while closing the driver: " + e.getMessage());
-            }
-        }
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-infobars"); // Bilgi çubuklarını devre dışı bırak
+        options.addArguments("--disable-notifications"); // Bildirimleri devre dışı bırak
+        options.addArguments("--start-maximized"); // Tarayıcıyı tam ekran başlat
+
+        // ChromeDriver'ı başlat
+        //WebDriver driver = new ChromeDriver();
+
+        // Google'ı aç
+        driver.get("https://www.google.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+
+        // Tarayıcıyı kapat (isteğe bağlı)
+        //driver.quit();
     }
 }
